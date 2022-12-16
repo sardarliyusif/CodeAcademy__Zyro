ad248e3d26a5acbd6258b7b95791ebc6226778fc
ca r213.16 <ca.r213.16@ca.local>
2022-12-16 12:30:27 +0400

    feat: added pages

---
+++ assets/js/app.js
@@ -0,0 +1,3 @@
+const hamburgerNav = document.querySelector('.hamburger-nav');
+const bars = document.querySelector('.bars');
+bars.addEventListener('click',() => hamburgerNav.classList.toggle('d-none'))
